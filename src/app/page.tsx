4cd8bd52f011198e778c7d7912731af44d6d29cb
bhavya <bhavyanimbark007@gmail.com>
2026-07-26 16:36:49 +0530

    Disable page caching completely

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ import { SiteContentProvider } from "@/lib/content";
 import Home from "@/components/Home";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default async function Page() {
-  const data = (await assembleContent()) as any;
+  const data = await assembleContent();
 
   return (
     <SiteContentProvider data={data}>
